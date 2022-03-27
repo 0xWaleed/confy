20fcea0af84b89cc298caf361cc1670df713e01e
0xWaleed <imwaleed@outlook.sa>
2022-03-27 22:45:21 +0300

    feat: add ability to load specific environment envy ✨

--- lib/src/envy.dart
+++ lib/src/envy.dart
@@ -2,11 +2,36 @@ import "dart:io";
 
 import "package:yaml/yaml.dart";
 
-dynamic _envyObject;
+dynamic _envyObject = {};
 
-envyLoad() {
-  final envyContent = File(".envy.yaml").readAsStringSync();
-  _envyObject = loadYaml(envyContent);
+typedef EnvironmentResolverFunction = String? Function();
+
+final files = [".envy", ".envy.%s"];
+final extensions = ["yaml", "yml"];
+
+_loadEnvy(envyFile) {
+  String? file;
+  for (final ext in extensions) {
+    var temp = "$envyFile.$ext";
+    if (File(temp).existsSync()) {
+      file = temp;
+      break;
+    }
+  }
+
+  if (file == null) {
+    return;
+  }
+
+  final envyContent = File(file).readAsStringSync();
+  final envyContentAsYaml = loadYaml(envyContent);
+  _envyObject = {..._envyObject as Map, ...envyContentAsYaml};
+}
+
+envyLoad({EnvironmentResolverFunction? environmentResolver}) {
+  for (final file in files) {
+    _loadEnvy(file.replaceFirst("%s", (environmentResolver ?? () => "")()!));
+  }
 }
 
 T? envy<T>(String key, {T? defaultValue}) {
--- test/envy_test.dart
+++ test/envy_test.dart
@@ -1,1 +1,149 @@
-void main() {}
+import "dart:io";
+
+import "package:envy/envy.dart";
+import "package:test/test.dart";
+
+late String envyFileExtension;
+
+testBasicUsage() {
+  test("load envy top level key", () {
+    envyLoad();
+    expect(envy("NAME"), equals("WALEED"));
+  });
+
+  test("load envy nested level key", () {
+    envyLoad();
+    expect(envy("SECRET.TOKEN"), equals("321"));
+  });
+
+  test("return null when top level key is not exist", () {
+    envyLoad();
+    expect(envy("not-exist"), isNull);
+  });
+
+  test("return null when nested level key is not exist", () {
+    envyLoad();
+    expect(envy("top.nested"), isNull);
+  });
+
+  test("can provide a default value top level key", () {
+    envyLoad();
+    expect(envy("not-exist", defaultValue: 99), equals(99));
+  });
+
+  test("can provide a default value nested key", () {
+    envyLoad();
+    expect(envy("not.exist", defaultValue: 99), equals(99));
+  });
+}
+
+testSpecificEnvironment() {
+  test("can load specific environment envy file", () {
+    envyLoad(environmentResolver: () => "dev");
+    expect(envy("NAME"), equals("WALEED"));
+  });
+
+  test("load envy nested level key", () {
+    envyLoad(environmentResolver: () => "dev");
+    expect(envy("SECRET.KEY"), equals(123));
+  });
+
+  test("return null when top level key is not exist", () {
+    envyLoad(environmentResolver: () => "dev");
+    expect(envy("not-exist"), isNull);
+  });
+
+  test("return null when nested level key is not exist", () {
+    envyLoad(environmentResolver: () => "dev");
+    expect(envy("top.nested"), isNull);
+  });
+
+  test("can provide a default value top level key", () {
+    envyLoad(environmentResolver: () => "dev");
+    expect(envy("not-exist", defaultValue: 99), equals(99));
+  });
+
+  test("can provide a default value nested key", () {
+    envyLoad(environmentResolver: () => "dev");
+    expect(envy("not.exist", defaultValue: 99), equals(99));
+  });
+}
+
+testWhenNoEnvyFileExist() {
+  test("should not throw when file is not exist", () {
+    expect(() => envyLoad(), returnsNormally);
+  });
+}
+
+testWhenBothFileExist() {
+  test("should load both files", () {
+    envyLoad(environmentResolver: () => "dev");
+    expect(envy("NAME"), equals("WALEED"));
+    expect(envy("SECRET.SEED"), equals("8-8"));
+  });
+
+  test("specific file override loaded keys", () {
+    envyLoad(environmentResolver: () => "dev");
+    expect(envy("SECRET.TOKEN"), equals("444"));
+  });
+}
+
+main() {
+  group(".yaml", () {
+    envyFileExtension = "yaml";
+    setUpAll(() {
+      File(".envy.$envyFileExtension").writeAsStringSync("""
+    NAME: WALEED
+    SECRET:
+      KEY: 123
+      TOKEN: "321"
+    """);
+
+      File(".envy.dev.$envyFileExtension").writeAsStringSync("""
+    NAME: WALEED
+    SECRET:
+      KEY: 123
+      TOKEN: "444"
+      SEED: "8-8"
+    """);
+    });
+
+    group("basic usage", testBasicUsage);
+    group("specific environment", testSpecificEnvironment);
+    group("when no envy file exist", testWhenNoEnvyFileExist);
+    group("when both file exist", testWhenBothFileExist);
+
+    tearDownAll(() {
+      File(".envy.$envyFileExtension").deleteSync();
+      File(".envy.dev.$envyFileExtension").deleteSync();
+    });
+  });
+
+  group(".yml", () {
+    envyFileExtension = "yml";
+    setUpAll(() {
+      File(".envy.$envyFileExtension").writeAsStringSync("""
+    NAME: WALEED
+    SECRET:
+      KEY: 123
+      TOKEN: "321"
+    """);
+
+      File(".envy.dev.$envyFileExtension").writeAsStringSync("""
+    NAME: WALEED
+    SECRET:
+      KEY: 123
+      TOKEN: "444"
+      SEED: "8-8"
+    """);
+    });
+    group("basic usage", testBasicUsage);
+    group("specific environment", testSpecificEnvironment);
+    group("when no envy file exist", testWhenNoEnvyFileExist);
+    group("when both file exist", testWhenBothFileExist);
+    tearDownAll(() {
+      File(".envy.$envyFileExtension").deleteSync();
+      File(".envy.dev.$envyFileExtension").deleteSync();
+    });
+  });
+}
